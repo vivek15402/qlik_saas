3d523f7f9c5a5234d06d4b0b8d8356c1ea4788bb
Vivek Saini <44522137+vivek15402@users.noreply.github.com>
2023-05-21 14:26:29 +0530

    Commit

--- Brightly_Welcome.js
+++ Brightly_Welcome.js
@@ -7,27 +7,7 @@ var config = {
 };
 
 //Redirect to login if user is not logged in
-async function login() {
-    function isLoggedIn() {
-        return fetch("https://"+config.host+"/api/v1/users/me", {
-            method: 'GET',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-                'qlik-web-integration-id': config.webIntegrationId,
-            },
-        }).then(response => {
-            return response.status === 200;
-        });
-    }
-    return isLoggedIn().then(loggedIn =>{
-        if (!loggedIn) {
-            window.location.href = "https://"+config.host+"/login?qlik-web-integration-id=" + config.webIntegrationId + "&returnto=" + location.href;
-            throw new Error('not logged in');
-        }
-    });
-}
+
 login().then(() => {
     require.config( {
         baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
