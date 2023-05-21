c6eec5db863b2644365b3ecf4a930eff012db662
Vivek Saini <44522137+vivek15402@users.noreply.github.com>
2023-05-21 14:27:22 +0530

    Cmt

--- Brightly_Welcome.js
+++ Brightly_Welcome.js
@@ -8,30 +8,25 @@ var config = {
 
 //Redirect to login if user is not logged in
 
-login().then(() => {
-    require.config( {
-        baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
-        webIntegrationId: config.webIntegrationId
-    } );
-    //Load js/qlik after authentication is successful
-    require( ["js/qlik"], function ( qlik ) {
-        qlik.on( "error", function ( error ) {
-            $( '#popupText' ).append( error.message + "<br>" );
-            $( '#popup' ).fadeIn( 1000 );
-        } );
-        $( "#closePopup" ).click( function () {
-            $( '#popup' ).hide();
-        } );
-        //open apps -- inserted here --
-        var app = qlik.openApp( 'b34d663a-5eae-42eb-b007-c0565426c233', config );
-       
-        //get objects -- inserted here --
-        app.visualization.get('CSxZqS').then(function(vis){
+
+//Load js/qlik after authentication is successful
+require(["js/qlik"], function (qlik) {
+    qlik.on("error", function (error) {
+        $('#popupText').append(error.message + "<br>");
+        $('#popup').fadeIn(1000);
+    });
+    $("#closePopup").click(function () {
+        $('#popup').hide();
+    });
+    //open apps -- inserted here --
+    var app = qlik.openApp('b34d663a-5eae-42eb-b007-c0565426c233', config);
+
+    //get objects -- inserted here --
+    app.visualization.get('CSxZqS').then(function (vis) {
         vis.show("QV01");
-        } );
-        app.visualization.get('jUHbaS').then(function(vis){
+    });
+    app.visualization.get('jUHbaS').then(function (vis) {
         vis.show("QV02");
-        } );
-	    
-    } );
+    });
+
 });
