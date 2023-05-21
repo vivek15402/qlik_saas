5b460ef4b7ef4b4eb89ca2a3421f9d0d5b8a0ff2
Vivek Saini <44522137+vivek15402@users.noreply.github.com>
2023-05-21 14:35:16 +0530

    Commit

--- Brightly_Welcome.js
+++ Brightly_Welcome.js
@@ -7,26 +7,51 @@ var config = {
 };
 
 //Redirect to login if user is not logged in
-
-
-//Load js/qlik after authentication is successful
-require(["js/qlik"], function (qlik) {
-    qlik.on("error", function (error) {
-        $('#popupText').append(error.message + "<br>");
-        $('#popup').fadeIn(1000);
+async function login() {
+    function isLoggedIn() {
+        return fetch("https://"+config.host+"/api/v1/users/me", {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'qlik-web-integration-id': config.webIntegrationId,
+            },
+        }).then(response => {
+            return response.status === 200;
+        });
+    }
+    return isLoggedIn().then(loggedIn =>{
+        if (!loggedIn) {
+            window.location.href = "https://"+config.host+"/login?qlik-web-integration-id=" + config.webIntegrationId + "&returnto=" + location.href;
+            throw new Error('not logged in');
+        }
     });
-    $("#closePopup").click(function () {
-        $('#popup').hide();
-    });
-    //open apps -- inserted here --
-    var app = qlik.openApp('b34d663a-5eae-42eb-b007-c0565426c233', config);
-
-    //get objects -- inserted here --
-    app.visualization.get('CSxZqS').then(function (vis) {
+}
+login().then(() => {
+    require.config( {
+        baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
+        webIntegrationId: config.webIntegrationId
+    } );
+    //Load js/qlik after authentication is successful
+    require( ["js/qlik"], function ( qlik ) {
+        qlik.on( "error", function ( error ) {
+            $( '#popupText' ).append( error.message + "<br>" );
+            $( '#popup' ).fadeIn( 1000 );
+        } );
+        $( "#closePopup" ).click( function () {
+            $( '#popup' ).hide();
+        } );
+        //open apps -- inserted here --
+        var app = qlik.openApp( 'b34d663a-5eae-42eb-b007-c0565426c233', config );
+       
+        //get objects -- inserted here --
+        app.visualization.get('CSxZqS').then(function(vis){
         vis.show("QV01");
-    });
-    app.visualization.get('jUHbaS').then(function (vis) {
+        } );
+        app.visualization.get('jUHbaS').then(function(vis){
         vis.show("QV02");
-    });
-
+        } );
+	    
+    } );
 });
